ab0ef2e151b236e34d19d023c1bc748c2d91a9e0
Thijsvanede <t.s.vanede@utwente.nl>
2020-05-21 14:31:14 -0700

    Cleanup

--- deeplog/__main__.py
+++ deeplog/__main__.py
@@ -27,13 +27,13 @@ if __name__ == "__main__":
     group_input.add_argument('file', help='file to read as input')
     group_input.add_argument('-f', '--field' , default='threat_name', help='FIELD to extract from input FILE')
     group_input.add_argument('-m', '--max'   , type=float, default=float('inf'), help='maximum number of items to read from input')
-    group_input.add_argument('-l', '--window', type=int  , default=10          , help="length of input sequence")
+    group_input.add_argument('-w', '--window', type=int  , default=10          , help="length of input sequence")
 
     # Deeplog parameters
     group_deeplog = parser.add_argument_group("Tiresias parameters")
     group_deeplog.add_argument(      '--hidden', type=int, default=64 , help='hidden dimension')
     group_deeplog.add_argument('-i', '--input' , type=int, default=300, help='input  dimension')
-    group_deeplog.add_argument(      '--layers', type=int, default=2  , help='number of lstm layers to use')
+    group_deeplog.add_argument('-l', '--layers', type=int, default=2  , help='number of lstm layers to use')
     group_deeplog.add_argument('-k', '--top'   , type=int, default=1  , help='accept any of the TOP predictions')
 
     # Training
@@ -43,27 +43,6 @@ if __name__ == "__main__":
     group_training.add_argument('-e', '--epochs'    , type=int, default=10,    help="number of epochs to train with")
     group_training.add_argument('-r', '--random'    , action='store_true',     help="train with random selection")
     group_training.add_argument(      '--ratio'     , type=float, default=0.5, help="proportion of data to use for training")
-
-
-    # # Add arguments
-    # parser.add_argument('test' , type=str, nargs='+', help="file(s) from which to read test data.")
-    #
-    # # Add DeepLog arguments
-    # parse_group_deeplog = parser.add_argument_group("Optional DeepLog parameters")
-    # parse_group_deeplog.add_argument('-b', '--batch-size', type=int, default=128, help="batch size for training")
-    # parse_group_deeplog.add_argument('-d', '--hidden-dim', type=int, default= 64, help="dimension of hidden layer")
-    # parse_group_deeplog.add_argument('-e', '--epochs'    , type=int, default= 10, help="number of epochs to train with")
-    # parse_group_deeplog.add_argument('-l', '--layers'    , type=int, default=  2, help="number of layers used by DeepLog")
-    # parse_group_deeplog.add_argument('-o', '--output'    , type=int, default=  0, help="required output shape")
-    # parse_group_deeplog.add_argument('-w', '--window'    , type=int, default= 10, help="window size used by DeepLog")
-    # parse_group_deeplog.add_argument('-t', '--top'       , type=int, default=1  , help='accept any of the TOP predictions')
-    #
-    # # Add input/output arguments
-    # parse_group_io = parser.add_argument_group("Optional I/O parameters")
-    # parse_input    = parse_group_io.add_mutually_exclusive_group(required=True)
-    # parse_input   .add_argument('--train', type=str, nargs='+', help="path to train data")
-    # parse_input   .add_argument('--load' , type=str,            help="path to load trained model")
-    # parse_group_io.add_argument('--save' , type=str,            help="path to save trained model")
 
     # Parse given arguments
     args = parser.parse_args()
@@ -94,45 +73,19 @@ if __name__ == "__main__":
     y_test  = data.get(args.field).get('test' ).get('y').to(device).reshape(-1)
 
     # # Test data
-    # X_test, y_test = list(), list()
-    # for i, test in enumerate(args.test):
-    #     X_test_ , y_test_ = Reader().read(test, args.window)
-    #     X_test.append(X_test_)
-    #     y_test.append(y_test_)
-    # X_test = torch.cat(X_test)
-    # y_test = torch.cat(y_test)
-    # # Store labels
-    # labels = y_test
+    # X_test, y_test= Reader().read('data/hdfs_test_normal', args.window)
+    # # Select maximum of test data
+    # X_test = X_test[:int(args.max)]
+    # y_test = y_test[:int(args.max)]
     #
-    # # Train from data
-    # if args.train:
-    #     # Read train data from given files
-    #     X_train, y_train = list(), list()
-    #     for train in args.train:
-    #         X_train_ , y_train_ = Reader().read(train, args.window)
-    #         X_train.append(X_train_)
-    #         y_train.append(y_train_)
-    #     X_train = torch.cat(X_train)
-    #     y_train = torch.cat(y_train)
-    #     # Get all labels
-    #     labels = torch.cat((y_train, labels))
+    # # Read train data from given files
+    # X_train, y_train = Reader().read('data/hdfs_train', args.window)
     #
     # # Encode labels
     # le = LabelEncoder()
-    # le = le.fit(labels)
-    # from time import time
-    # start = time()
+    # le = le.fit(torch.cat((y_test, y_train)))
     # y_test  = torch.tensor(le.transform(y_test))
-    #
-    # if args.train:
-    #     y_train = torch.tensor(le.transform(y_train))
-    #     # Get data as dataset
-    #     data = DataLoader(
-    #         TensorDataset(X_train, y_train),
-    #         args.batch_size,
-    #         shuffle=False,
-    #         pin_memory=True
-    #     )
+    # y_train = torch.tensor(le.transform(y_train))
 
     ########################################################################
     #                            Create DeepLog                            #
@@ -158,7 +111,7 @@ if __name__ == "__main__":
     ########################################################################
 
     # Initialise predictions
-    y_pred_top = y_pred[:, 0].clone()
+    y_pred_top = y_pred[:, 0]
     # Compute top TOP predictions
     for top in range(1, args.top):
         # Get mask
