e943ea0230f75b1fe15b748a6885a2cd7b6a1ea4
Thijsvanede <t.s.vanede@utwente.nl>
2020-05-21 15:15:11 -0700

    Added setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deeplog",
+    version="0.0.1",
+    author="Thijs van Ede",
+    author_email="t.s.vanede@utwente.nl",
+    description="Pytorch implementation of Deeplog: Anomaly detection and diagnosis from system logs through deep learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Thijsvanede/DeepLog",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
