8382911f0eef326f0c84874640b9195b2117deb3
Thijs van Ede <t.s.vanede@utwente.nl>
2021-07-16 14:09:09 +0200

    Deanonymized setup

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="deeplog",
     version="0.0.1",
-    author="Anonymized",
-    author_email="Anonymized",
+    author="Thijs van Ede",
+    author_email="t.s.vanede@utwente.nl",
     description="Pytorch implementation of Deeplog: Anomaly detection and diagnosis from system logs through deep learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/anonymized/DeepLog",
+    url="https://github.com/Thijsvanede/DeepLog",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
