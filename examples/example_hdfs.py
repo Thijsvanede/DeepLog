86032848c9a0e84f8030d09dca0e21942facd272
Thijs van Ede <t.s.vanede@utwente.nl>
2022-07-19 16:53:01 +0200

    Updated example with classification report of anomalies

--- examples/example_hdfs.py
+++ examples/example_hdfs.py
@@ -1,6 +1,9 @@
 # Import pytorch
 import torch
 
+# Import classification report
+from sklearn.metrics import classification_report
+
 # Import DeepLog and Preprocessor
 from deeplog              import DeepLog
 from deeplog.preprocessor import Preprocessor
@@ -11,7 +14,7 @@ from deeplog.preprocessor import Preprocessor
 
 # Create preprocessor for loading data
 preprocessor = Preprocessor(
-    length  = 20,           # Extract sequences of 20 items
+    length  = 10,           # Extract sequences of 20 items
     timeout = float('inf'), # Do not include a maximum allowed time between events
 )
 
@@ -26,9 +29,9 @@ X_test_anomaly, y_test_anomaly, label_test_anomaly, mapping_test_anomaly = prepr
 
 # Create DeepLog object
 deeplog = DeepLog(
-    input_size  = 300, # Number of different events to expect
+    input_size  = 30, # Number of different events to expect
     hidden_size = 64 , # Hidden dimension, we suggest 64
-    output_size = 300, # Number of different events to expect
+    output_size = 30, # Number of different events to expect
 )
 
 # Optionally cast data and DeepLog to cuda, if available
@@ -48,21 +51,34 @@ if torch.cuda.is_available():
 deeplog.fit(
     X          = X_train,
     y          = y_train,
-    epochs     = 100,
+    epochs     = 10,
     batch_size = 128,
+    optimizer  = torch.optim.Adam,
 )
 
 # Predict normal data using deeplog
 y_pred_normal, confidence = deeplog.predict(
     X = X_test,
-    k = 3, # Change this value to get the top k predictions (called 'g' in DeepLog paper, see Figure 6)
+    k = 9, # Change this value to get the top k predictions (called 'g' in DeepLog paper, see Figure 6)
 )
 
 # Predict anomalous data using deeplog
 y_pred_anomaly, confidence = deeplog.predict(
     X = X_test_anomaly,
-    k = 3, # Change this value to get the top k predictions (called 'g' in DeepLog paper, see Figure 6)
+    k = 9, # Change this value to get the top k predictions (called 'g' in DeepLog paper, see Figure 6)
 )
+
+################################################################################
+#                            Classification report                             #
+################################################################################
+
+print("Classification report - predictions")
+print(classification_report(
+    y_true = y_test.cpu().numpy(),
+    y_pred = y_pred_normal[:, 0].cpu().numpy(),
+    digits = 4,
+    zero_division = 0,
+))
 
 ################################################################################
 #                             Check for anomalies                              #
@@ -84,8 +100,18 @@ anomalies_abnormal = ~torch.any(
     dim = 0,
 )
 
-# Print the fraction of anomalies in normal data (False positives)
-print(f"False positives: {anomalies_normal.sum() / anomalies_normal.shape[0]}")
+# Compute classification report for anomalies
+y_pred = torch.cat((anomalies_normal, anomalies_abnormal))
+y_true = torch.cat((
+    torch.zeros(anomalies_normal  .shape[0], dtype=bool),
+    torch.ones (anomalies_abnormal.shape[0], dtype=bool),
+))
 
-# Print the fraction of anomalies in abnormal data (True positives)
-print(f"True  positives: {anomalies_abnormal.sum() / anomalies_abnormal.shape[0]}")
+print("Classification report - anomalies")
+print(classification_report(
+    y_pred       = y_pred.cpu().numpy(),
+    y_true       = y_true.cpu().numpy(),
+    labels       = [False, True],
+    target_names = ["Normal", "Anomaly"],
+    digits       = 4,
+))
